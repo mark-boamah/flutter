92885b87f9753a21b5677ca1de092d9576a6a5cc
Boamah Mark (s112576) <mark.boamah@student.ap.be>
2022-03-09 19:23:20 +0100

    adding all flutter apps I have made so far

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import './quiz.dart';
+import './result.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What\s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Snake', 'score': 10},
+        {'text': 'Elephant', 'score': 10},
+        {'text': 'Lion', 'score': 10}
+      ]
+    },
+    {
+      'questionText': 'Who\s your favorite instructor?',
+      'answers': [
+        {'text': 'Mark', 'score': 50},
+        {'text': 'Robin', 'score': 10},
+        {'text': 'Frank', 'score': 10},
+        {'text': 'Lina', 'score': 10}
+      ]
+    },
+  ];
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions');
+    }
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
